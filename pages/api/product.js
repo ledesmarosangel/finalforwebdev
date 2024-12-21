91b40fe9b10749508ef6ee7f305733c3ad37f42a
ledesmarosangel <137957251+ledesmarosangel@users.noreply.github.com>
2024-12-21 15:55:45 -0400

    Update product.js

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,8 +1,22 @@
-    import {initMongoose} from "../../lib/mongoose";
-    import Product from "../../models/Product"
+import { initMongoose } from "../../lib/mongoose";
+import Product from "../../models/Product";
 
-export default async function handle(req, res){
+export async function findAllProducts() {
+    return Product.find().exec();
+}
+
+export default async function handle(req, res) {
     await initMongoose();
-    res.json( await Product.find().exec());
-    
+    const { ids } = req.query;
+
+    if (ids) {
+        const idsArray = ids.split(','); // Split the comma-separated IDs into an array
+        res.json(
+            await Product.find({
+                '_id': { $in: idsArray } // Use the $in operator to filter by multiple IDs
+            }).exec()
+        );
+    } else {
+        res.json(await findAllProducts()); // If no IDs are provided, return all products
+    }
 }
