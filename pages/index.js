17a2e65730e945f7f69e6120584fc4f37b009aed
ledesmarosangel <137957251+ledesmarosangel@users.noreply.github.com>
2024-12-21 15:54:24 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,71 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useEffect, useState } from "react";
+import Product from "../components/Product";
+import {initMongoose} from "../lib/mongoose";
+import {findAllProducts} from "./api/product";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
 
-export default function Home() {
+
+
+
+
+export default function Home({products}) {
+  
+  const [phrase, setPhrase] = useState([]);
+ 
+  const categoriesNames = [...new Set(products.map(p => p.category))];
+
+  if(phrase){
+      products = products.filter(p => p.name.toLowerCase().includes(phrase));
+  }
+ 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  <header>
+        <h1 className="text-3xl font-bold text-center">Sunflower Haven</h1>
+      
+      </header>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+       
+      <input value={phrase} onChange={e => setPhrase(e.target.value)}  type="text" placeholder="Search for products..." className="bg-gray-100 w-full py-2 px-4 rounded-xl"/>
+      <div>
+        {categoriesNames.map(categoryName => (
+          <div key={categoryName}>
+              {products.find(p => p.category === categoryName) && (
+                <div>
+                  <h2 className="text-2xl capitalize text-center">{categoryName}</h2>
+            <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide bg-gray-200 items-center">
+            {products
+              .filter(p => p.category === categoryName)
+              .map(productInfo =>(
+              <div key={productInfo._id} className="px-5 snap-start bg-gray-200 ">
+                <Product {...productInfo} />
+              </div>
+            ))}
+            </div>
+                </div>
+              )}
+            
+          </div>
+        ))}
+       
         </div>
-      </main>
+    </Layout>
+  )
+};
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
-}
+
+export async function getServerSideProps(){
+
+  await initMongoose();
+  const products = await findAllProducts();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+  }
+  
