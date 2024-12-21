d5484414930f0d430e3ffd2fe7de9d4c2610e369
ledesmarosangel <137957251+ledesmarosangel@users.noreply.github.com>
2024-12-21 15:39:27 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,27 @@
 import Footer from "./Footer";
-export default function Layout({children}){
+import {useContext, useEffect, useState} from "react";
+import {ProductsContext} from "./ProductsContext";
 
-    return (
-        <div>
-            <div className="p-5">
-                {children}
-            </div>
-            <Footer />
-        </div>
-    );
+export default function Layout({children}) {
+  const {setSelectedProducts} = useContext(ProductsContext);
+  const [success,setSuccess] = useState(false);
+  useEffect(() => {
+    if (window.location.href.includes('success')) {
+      setSelectedProducts([]);
+      setSuccess(true);
+    }
+  }, []);
+  return (
+    <div>
+      <div className="p-5">
+        {success && (
+          <div className="text-center mb-5 bg-yellow-600 text-white text-lg p-5 rounded-xl">
+            Thanks for your order!
+          </div>
+        )}
+        {children}
+      </div>
+      <Footer />
+    </div>
+  );
 }
