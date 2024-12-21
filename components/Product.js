cb4a214ecbd74da6604b8d1284a1d5de03058a58
ledesmarosangel <137957251+ledesmarosangel@users.noreply.github.com>
2024-12-21 15:40:30 -0400

    Update Product.js

--- components/Product.js
+++ components/Product.js
@@ -1,17 +1,25 @@
-export default function Product({name,price,description,picture}){
-    return(
+import {useContext} from "react";
+import { ProductsContext } from "./ProductsContext";
+
+
+export default function Product({_id,name,price,description,picture}){
+   const {setSelectedProducts} = useContext(ProductsContext);
+   function addProduct() {
+    setSelectedProducts(prev => [...prev,_id]);
+    
+   }
+  return(
     <div className="w-64">
-    <div className="bg-blue-100 p-5 rounder-xl">
+    <div className="bg-gray-200 p-5 rounded-xl items-center">
    <img src={picture} alt=""/>
     </div>
     <div className="mt-2"> 
       <h3 className= "font-bold text-lg">{name}</h3>
    </div>
-   <p className="text-sm mt-1 leading-4">{description}</p>
+   <p className="text-sm mt-1 leading-4 text-gray-500">{description}</p>
    <div className="flex mt-1">
       <div className="text-2xl font-bold grow">${price}</div>
-      <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl"> + </button>
+      <button onClick={addProduct} className="bg-yellow-500 text-white py-1 px-3 rounded-xl"> + </button>
   </div>
 </div>
 );
-}
